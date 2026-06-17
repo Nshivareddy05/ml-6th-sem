0c7f93c29b2ffc8b50e59ffd47efaa76460c1f43
N Shiva Manohara Reddy <164047244+Nshivareddy05@users.noreply.github.com>
2026-06-17 22:06:14 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Titanic-Dataset.csv")
+
+num_col = "Age"
+
+data=df[num_col].dropna()
+
+print("Mean: ", data.mean())
+print("Median: ", data.median())
+print("Mode: ", data.mode()[0])
+print("Standard Deviation: ", data.std())
+print("Variance: ", data.var())
+print("Range: ", data.max() - data.min())
+
+plt.hist(data, bins=10)
+plt.title("Boxplot of " + num_col)
+plt.show()
+
+Q1 = data.quantile(0.25)
+Q3 = data.quantile(0.75)
+IQR = Q3 - Q1
+
+lower = Q1 - 1.5 * IQR
+upper = Q3 + 1.5 * IQR
+
+outliers = data[(data < lower) | (data > upper)]
+print("Outliers:\n", outliers)
+
+cat_col = "Sex"
+
+freq = df[cat_col].value_counts()
+print("\nCatergory Frequency:\n", freq)
+
+freq.plot(kind='bar')
+plt.title("Bar Chart of " + cat_col)
+plt.show()
